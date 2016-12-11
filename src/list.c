3bdffc15a4a01e51d6e4ac7e7aa8ac0505412dee
v1c0nt <v1c0nt@d95e66fd-9402-4a7b-af37-a488d08096bb>
2016-12-11 09:38:03 +0000

    prepare to release
    git-svn-id: svn://svn.code.sf.net/p/yad-dialog/code/trunk@1069 d95e66fd-9402-4a7b-af37-a488d08096bb

--- src/list.c
+++ src/list.c
@@ -750,25 +750,25 @@ add_row_cb (GtkMenuItem * item, gpointer data)
 
   if (options.list_data.add_action)
     {
-          gchar *data = NULL;
-          gint exit;
+      gchar *data = NULL;
+      gint exit;
 
-          g_spawn_command_line_sync (options.list_data.add_action, &data, NULL, &exit, NULL);
-          if (exit == 0)
+      g_spawn_command_line_sync (options.list_data.add_action, &data, NULL, &exit, NULL);
+      if (exit == 0)
+        {
+          guint i, n_cols = gtk_tree_model_get_n_columns (model);
+          gchar **lines = g_strsplit (data, "\n", 0);
+
+          for (i = 0; i < n_cols; i++)
             {
-              guint i, n_cols = gtk_tree_model_get_n_columns (model);
-              gchar **lines = g_strsplit (data, "\n", 0);
+              if (lines[i] == NULL)
+                break;
 
-              for (i = 0; i < n_cols; i++)
-                {
-                  if (lines[i] == NULL)
-                    break;
-
-                  cell_set_data (&iter, i, lines[i]);
-                }
-              g_strfreev (lines);
+              cell_set_data (&iter, i, lines[i]);
             }
-          g_free (data);
+          g_strfreev (lines);
+        }
+      g_free (data);
     }
 }
 
